fb5a0a5f31fc8377ed0801b00df45e644aa6478a
senakurtak <sena.kurtak@wunder.is>
2023-05-11 23:33:00 +0300

    Initial Commit

---
+++ SideMenuTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuTesting
+//
+//  Created by Sena Kurtak on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
